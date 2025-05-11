a5e7059c74e4ad62ddc8d33ae1982950d73cc217
Valentina <vardilaq@unal.edu.co>
2025-05-11 13:36:40 -0500

    Tarea Completada

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,6 +5,7 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 # pylint: disable=import-outside-toplevel
 
 
+
 def pregunta_01():
     """
     Construya y retorne un dataframe de Pandas a partir del archivo
@@ -18,3 +19,48 @@ def pregunta_01():
 
 
     """
+    
+    import pandas as pd
+    df_dict = {
+      "cluster": [],
+      "cantidad_de_palabras_clave": [],
+      "porcentaje_de_palabras_clave": [],
+      "principales_palabras_clave": []
+    }
+    
+    with open("./files/input/clusters_report.txt", "r") as f:
+      lines = list(f.readlines())
+      lines = [line.strip("\n").replace(" %", "").split() for line in lines]
+      cluster = int()
+      num_of_keywords = int()
+      percentage = float()
+      main_keywords = ""
+      for line in lines[4:]:
+          if len(line) == 0:
+              df_dict["cluster"].append(cluster)
+              df_dict["cantidad_de_palabras_clave"].append(num_of_keywords)
+              df_dict["porcentaje_de_palabras_clave"].append(percentage)
+              df_dict["principales_palabras_clave"].append(main_keywords[:-1])
+              cluster = int()
+              num_of_keywords = int()
+              percentage = float()
+              main_keywords = ""
+              continue
+          try:
+              cluster = int(line[0])
+              num_of_keywords = int(line[1])
+              percentage = float(line[2].replace(",", "."))
+              for word in line[3:]:
+                  main_keywords += word.replace(".", "") + " "
+          except:
+              for word in line:
+                  main_keywords += word.replace(".", "") + " "
+      return pd.DataFrame().from_dict(df_dict)
+      
+    
+    
+  
+  
+
+if __name__ == "__main__":
+    print(pregunta_01())
